eb68eb44edbe81df655ce62f0d55b2c95b5980bc
True Ghiassi <true@ghiassitrio.co.uk>
2017-03-15 23:24:06 +0000

    I have made the function that generates the search path.

--- src/Main.hs
+++ src/Main.hs
@@ -18,31 +18,80 @@
 -- Public License along with Vis.  If not, see
 -- <http://www.gnu.org/licenses/>.
 
--- TEMP NOTES
--- The required endpoint of this module is a function
--- that will take a large image, a small image which
--- is an approximate subset of it, and a starting point, 
--- and find the location of the small image in the large one,
--- using a spiral search from the starting point.
-
-
 module Main where
 
-import qualified Data.Either as De
-import Vision.Image
-import Vision.Image.Storage.DevIL (Autodetect (..), load, save, StorageError)
-import Vision.Primitive (ix2)
+-- import qualified Data.Vector as Dv
+import qualified Data.List as Dl
+import qualified Data.Vector.Storable as Dvs
+import qualified Data.Word as Dw
+import qualified Vision.Image as Vi
+import qualified Vision.Image.Storage.DevIL as V
+-- import qualified Vision.Primitive.Shape as Vps
 
 main :: IO ()
-main = do 
-  pic <- load Autodetect "/w/git/vis/1.png"
-  let output = processImage pic
-  err <- save Autodetect "/w/git/vis/op.png" output
-  errorHandler err
+main = load >>= maybeSave >>= printError
 
-errorHandler :: Maybe StorageError -> IO ()
-errorHandler (Just _) = 
-  putStrLn "Writing to file failed, I'm afraid."
-errorHandler Nothing = putStrLn "OK, it worked."
+-- It searches for a small image in a big one given a 
+-- starting point.
+-- search :: Vi.RGB -> Vi.RGB -> (Int,Int) -> (Int,Int)
+-- search small big (x,y) = (x,y)  
 
-processImage = id
+searchPath :: (Int,Int) -> [(Int,Int)]
+searchPath start = no_indices
+  where 
+    no_indices = [x | (_,x) <- with_indices]
+    with_indices = Dl.unfoldr f (0,start)
+    f :: (Int,(Int,Int)) -> Maybe ((Int,(Int,Int)),(Int,(Int,Int)))
+    f (i,item) = Just ((i,item),(i+1, ((listOfMoves !! i) item)))
+
+-- It generates a sequence: [1,1,2,2,3,3,4,4,5,5...]
+mvDiffs :: [Int]
+mvDiffs = Dl.concatMap (replicate 2) [1..]
+
+listOfMoves :: [(Int,Int) -> (Int,Int)]
+listOfMoves = 
+  Dl.concat [take x (repeat m) | (x,m) <- zip mvDiffs mvs]
+  where 
+    mvs = Dl.cycle [right,up,left,down]
+    right (x,y) = (x+1,y)
+    up (x,y) = (x,y+1)
+    left (x,y) = (x-1,y)
+    down (x,y) = (x,y-1)
+
+-- It compares two equally-sized images and works out the
+-- least-squared difference between them.
+compare :: Vi.RGB -> Vi.RGB -> Int
+compare one two = 
+  Dvs.sum (Dvs.zipWith comparePixel first second)
+  where
+    first = Vi.manifestVector one
+    second = Vi.manifestVector two
+
+-- It calculates the least-squared difference between two
+-- RGB pixels.
+comparePixel :: Vi.RGBPixel -> Vi.RGBPixel -> Int
+comparePixel one two = red*red + green*green + blue*blue
+  where
+    red = f $ (Vi.rgbRed one) - (Vi.rgbRed two)
+    green = f $ (Vi.rgbGreen one) - (Vi.rgbGreen two)
+    blue = f $ (Vi.rgbBlue one) - (Vi.rgbBlue two)
+    f :: Dw.Word8 -> Int
+    f = fromIntegral
+
+load :: IO (Either V.StorageError Vi.RGB)
+load = V.load V.Autodetect "/w/git/vis/pinkblack2x2.tiff"
+
+maybeSave :: Either V.StorageError Vi.RGB 
+          -> IO (Maybe V.StorageError)
+maybeSave (Left err) = return (Just err)
+maybeSave (Right array) = save . processImage $ array
+
+printError :: Maybe V.StorageError -> IO () 
+printError (Just err) = putStrLn $ "Error: " ++ show err
+printError Nothing = putStrLn "All done."
+
+save :: Vi.RGB -> IO (Maybe V.StorageError)
+save = V.save V.Autodetect "/w/git/vis/opp.png" 
+
+processImage :: Vi.RGB -> Vi.RGB
+processImage = id 
